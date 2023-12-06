70cf68ec93ae77b07222322f18629405f11fba36
SatvikaChepyala <146900469+SatvikaChepyala@users.noreply.github.com>
2023-12-05 20:36:09 -0500

    second commit

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1>{{ CompanyTitle }}</h1>
+      <br>
+      <h4>{{ Manager }}</h4>
+  </div>
+</template>
+<script>
+export default {
+  name: "MyHeader",
+  props: {
+      CompanyTitle: String,
+      Manager: String
+  }
+}
+</script>
+<style scoped>
+div {
+
+  text-align: center;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  border: 2px solid;
+  background-color:  teal;
+}
+</style>
